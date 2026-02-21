df2c847ebc9094693c8353ad0b5088f2c97a1976
Alexey Sholokhov <asolohov@gmail.com>
2026-02-21 17:46:46 +0300

    исправлен цвет картинок в на странице историй

--- app/pages/about/stories.vue
+++ app/pages/about/stories.vue
@@ -8,15 +8,24 @@ definePageMeta({
 
 const iconCards: {
   id: string
-  color: string
   icon: Component
 }[] = [{
   id: '1',
-  color: 'green',
   icon: defineAsyncComponent(() => import('~/assets/svg/stories/girl-with-cat.svg')),
 }, {
   id: '2',
-  color: 'blue',
+  icon: defineAsyncComponent(() => import('~/assets/svg/stories/man-with-dog.svg')),
+}, {
+  id: '3',
+  icon: defineAsyncComponent(() => import('~/assets/svg/stories/girl-with-cat.svg')),
+}, {
+  id: '4',
+  icon: defineAsyncComponent(() => import('~/assets/svg/stories/man-with-dog.svg')),
+}, {
+  id: '5',
+  icon: defineAsyncComponent(() => import('~/assets/svg/stories/girl-with-cat.svg')),
+}, {
+  id: '6',
   icon: defineAsyncComponent(() => import('~/assets/svg/stories/man-with-dog.svg')),
 }]
 
@@ -48,7 +57,7 @@ const { data: storiesResponse } = await useAsyncData('stories-page', () => {
           <template #interspersed="{ data }">
             <div
               class="stories-page__interspersed"
-              :class="['stories-page__interspersed--' + data.color]"
+              :style="{ color: `var(--color-${data.color}-dark)` }"
             >
               <component
                 :is="data.icon"
@@ -73,14 +82,6 @@ const { data: storiesResponse } = await useAsyncData('stories-page', () => {
     justify-content: center;
     flex: 1;
     padding: 60% 0 0;
-
-    &--green {
-      color: var(--color-green-dark);
-    }
-
-    &--blue {
-      color: var(--color-blue-dark);
-    }
   }
 
   &__interspersed-icon {
